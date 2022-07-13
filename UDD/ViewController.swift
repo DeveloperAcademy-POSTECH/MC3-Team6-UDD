ec4062e448563f58627a32ccce1927843eda9986
minjae Kim <hjn966@naver.com>
2022-07-13 15:02:02 +0900

    Initial Commit

---
+++ UDD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UDD
+//
+//  Created by 김민재 on 2022/07/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
