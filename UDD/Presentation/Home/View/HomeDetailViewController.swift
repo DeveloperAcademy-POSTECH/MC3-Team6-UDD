09e6d5ae35ee64c842082bc4b7681c6408887721
jsh9611 <jangsh9611@naver.com>
2022-07-19 21:58:44 +0900

    [feat] add HomeDetailView

---
+++ UDD/Presentation/Home/View/HomeDetailViewController.swift
@@ -0,0 +1,58 @@
+//
+//  HomeDetailViewController.swift
+//  UDD
+//
+//  Created by SeongHoon Jang on 2022/07/19.
+//
+
+import UIKit
+
+class HomeDetailViewController: UIViewController {
+    @IBOutlet weak var dogImage: UIImageView!
+    @IBOutlet weak var dogName: UILabel!
+    @IBOutlet weak var dogTags: UILabel!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var dogWeightInfo: UILabel!
+    
+    var dogDummy: DogOwner = DogOwner.dummy
+    var isDisabled: Bool = DogOwner.personLike
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        dogImage.image = UIImage(named: dogDummy.dogImage)
+        dogName.text = dogDummy.dogName
+        dogTags.text = dogTagsString()
+        dogWeightInfo.text = dogSizeString(weight: dogDummy.dogWeight)
+    }
+    
+    @IBAction func likeButtonAction(_ sender: UIButton) {
+        changeLikeState()
+    }
+    
+    func changeLikeState() {
+        let imgName = isDisabled ? "heart" : "heart.fill"
+        isDisabled.toggle()
+        self.likeButton.setImage(UIImage(systemName: imgName), for: .normal)
+    }
+    
+    func dogTagsString() -> String {
+        var dogData = ""
+        for tagg in dogDummy.dogHashTags {
+            dogData += "#\(tagg)  "
+        }
+        return dogData
+    }
+    
+    func dogSizeString(weight: Double) -> String{
+        let dogSize : String
+        if weight < 10 {
+            dogSize = "소형견"
+        } else if weight < 25 {
+            dogSize = "중형견"
+        } else {
+            dogSize = "대형견"
+        }
+        return "\(weight) kg (\(dogSize))"
+    }
+}
+
