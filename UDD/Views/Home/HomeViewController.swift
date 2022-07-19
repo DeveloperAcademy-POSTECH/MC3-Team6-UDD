dc9e746c695ec401faff4d8cc9dfde6db5b1d8a2
minjae Kim <hjn966@naver.com>
2022-07-19 18:04:24 +0900

    [feat] add table view

---
+++ UDD/Views/Home/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  UDD
+//
+//  Created by 김민재 on 2022/07/19.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
