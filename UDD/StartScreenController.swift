fc6fe3f34e56471f3ff0d301e52207b418e5f0be
J2in <jiin62622@gmail.com>
2022-07-20 15:24:42 +0900

    [feat] add start screen storyboard

---
+++ UDD/StartScreenController.swift
@@ -0,0 +1,16 @@
+//
+//  StartScreenController.swift
+//  UDD
+//
+//  Created by jiin on 2022/07/20.
+//
+
+import UIKit
+
+class StartScreenController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
