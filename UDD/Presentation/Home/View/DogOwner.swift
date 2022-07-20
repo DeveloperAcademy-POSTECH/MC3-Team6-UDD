f16afd3e8bd99625f8d31e983d4d4b251c62a8c1
jsh9611 <jangsh9611@naver.com>
2022-07-20 16:09:16 +0900

    [feat] make view can get data

--- UDD/Presentation/Home/View/DogOwner.swift
+++ UDD/Presentation/Home/View/DogOwner.swift
@@ -13,12 +13,10 @@ struct DogOwner {
     let dogHashTags: [String]
     let dogWeight: Double
 //    let dogSex: String
-    
 }
 
 extension DogOwner {
     static let dummy: DogOwner =
     DogOwner(dogImage: "golddog", dogName: "야옹이", dogHashTags: ["소심한", "중심한", "대심한", "청심환"], dogWeight: 25.3)
-    
     static let personLike = false
 }
--- UDD/Presentation/Home/View/HomeDetailViewController.swift
+++ UDD/Presentation/Home/View/HomeDetailViewController.swift
@@ -13,28 +13,28 @@ class HomeDetailViewController: UIViewController {
     @IBOutlet weak var dogTags: UILabel!
     @IBOutlet weak var likeButton: UIButton!
     @IBOutlet weak var dogWeightInfo: UILabel!
+    @IBOutlet weak var dogInfo: UILabel!
+    @IBOutlet weak var personInfo: UILabel!
     
     var dogDummy: DogOwner = DogOwner.dummy
     var isDisabled: Bool = DogOwner.personLike
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         dogImage.image = UIImage(named: dogDummy.dogImage)
         dogName.text = dogDummy.dogName
         dogTags.text = dogTagsString()
         dogWeightInfo.text = dogSizeString(weight: dogDummy.dogWeight)
+        dogInfo.text = "정보 한줄 소개정보 한줄 소개정보 한줄 소개정보 한줄 소개정보 한줄 소개정보 한줄 소개정보 한줄 소개"
+        personInfo.text = "나칠땡  [24]"
     }
-    
     @IBAction func likeButtonAction(_ sender: UIButton) {
         changeLikeState()
     }
-    
     func changeLikeState() {
         let imgName = isDisabled ? "heart" : "heart.fill"
         isDisabled.toggle()
         self.likeButton.setImage(UIImage(systemName: imgName), for: .normal)
     }
-    
     func dogTagsString() -> String {
         var dogData = ""
         for tagg in dogDummy.dogHashTags {
@@ -42,8 +42,7 @@ class HomeDetailViewController: UIViewController {
         }
         return dogData
     }
-    
-    func dogSizeString(weight: Double) -> String{
+    func dogSizeString(weight: Double) -> String {
         let dogSize : String
         if weight < 10 {
             dogSize = "소형견"
@@ -55,4 +54,3 @@ class HomeDetailViewController: UIViewController {
         return "\(weight) kg (\(dogSize))"
     }
 }
-
