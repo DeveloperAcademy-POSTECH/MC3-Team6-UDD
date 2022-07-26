a7c0e0fba6bbcb4af78b0cdc331bb07c2e824117
Hongjaaa <102945279+Hongjaaa@users.noreply.github.com>
2022-07-26 17:43:35 +0900

    Add Segmented Control, TextView, TextViewCell

---
+++ UDD/Presentation/Main/View/Like/LikeListView.swift
@@ -0,0 +1,14 @@
+//
+//  LikeListView.swift
+//  UDD
+//
+//  Created by 최홍준 on 2022/07/26.
+//
+
+import UIKit
+
+class LikeListView: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
