6634dbe3510e584d6258314d6d75572dc27826f9
J2in <jiin62622@gmail.com>
2022-07-27 17:58:46 +0900

    [refactor] refactoring temp data

--- UDD/Presentation/Main/View/Chat/ChatListController.swift
+++ UDD/Presentation/Main/View/Chat/ChatListController.swift
@@ -14,17 +14,76 @@ class ChatCell: UITableViewCell {
     @IBOutlet weak var profileImage: UIImageView!
 }
 
-class ChatListController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+struct RecentMessage {
+    let senderName: String! // 보낸사람 강아지 이름
+    let senderImage: UIImage // 보낸사람 프로필 사진
+    let messageContext: String
+    let messageTime: Date
+}
 
-    let username = ["똘이 견주", "짱아 견주", "뫄뫄 견주", "어쩌구 견주주", "고양이 견주", "고양이 견주", "고양이 견주", "고양이 견주"]
-    let content = ["미친 척이라 말해 what's the loss to me? Ya",
-                   "사정없이 씹으라고 you're lost to me ya",
-                   "사랑 그깟 거 따위 내 눈에 눈물 한 방울 어림없지지지지지 베이비베이비베이비비에비",
-                   "너의 하찮은 말에 미소나 지을 바엔", "예 암 뻐킹 톰보이", "예 암 뻐킹 톰보이", "예 암 뻐킹 톰보이", "예 암 뻐킹 톰보이"]
-    let time = ["12:34","10:22","09:58","08:10","00:04","00:03","00:02","00:00"]
+class ChatListController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    func getChatTime (date: Date) -> String {
+        let time = DateFormatter()
+        time.dateFormat = "HH:mm"
+        let chatTimeString = time.string(from: date)
+        return chatTimeString
+    }
+    var messageList = [RecentMessage]()
+
+    func addList() {
+        messageList.append(
+            RecentMessage(senderName: "똘이",
+                          senderImage: UIImage(systemName: "doc.fill")!, // 이거 ! 가 아니라 다른걸로 해줄거임..!
+                          messageContext: "미친 척이라 말해 what's the loss to me? Ya",
+                          messageTime: Date())
+        )
+        messageList.append(
+            RecentMessage(senderName: "짱아",
+                          senderImage: UIImage(systemName: "doc.fill")!, // 이거 ! 가 아니라 다른걸로 해줄거임..!
+                          messageContext: "사랑 그깟 거 따위 내 눈에 눈물 한 방울 어림없지지지지지 베이비베이비베이비비에비",
+                          messageTime: Date())
+        )
+        messageList.append(
+            RecentMessage(senderName: "어쩌구저쩌구",
+                          senderImage: UIImage(systemName: "doc.fill")!, // 이거 ! 가 아니라 다른걸로 해줄거임..!
+                          messageContext: "사정없이 씹으라고 you're lost to me ya",
+                          messageTime: Date())
+        )
+        messageList.append(
+            RecentMessage(senderName: "망망망",
+                          senderImage: UIImage(systemName: "doc.fill")!, // 이거 ! 가 아니라 다른걸로 해줄거임..!
+                          messageContext: "미친 척이라 말해 what's the loss to me? Ya",
+                          messageTime: Date())
+        )
+        messageList.append(
+            RecentMessage(senderName: "몽몽이",
+                          senderImage: UIImage(systemName: "doc.fill")!, // 이거 ! 가 아니라 다른걸로 해줄거임..!
+                          messageContext: "예 암 뻐킹 톰보이",
+                          messageTime: Date())
+        )
+        messageList.append(
+            RecentMessage(senderName: "몽몽이",
+                          senderImage: UIImage(systemName: "doc.fill")!, // 이거 ! 가 아니라 다른걸로 해줄거임..!
+                          messageContext: "예 암 뻐킹 톰보이",
+                          messageTime: Date())
+        )
+        messageList.append(
+            RecentMessage(senderName: "몽몽이",
+                          senderImage: UIImage(systemName: "doc.fill")!, // 이거 ! 가 아니라 다른걸로 해줄거임..!
+                          messageContext: "예 암 뻐킹 톰보이",
+                          messageTime: Date())
+        )
+        messageList.append(
+            RecentMessage(senderName: "몽몽이",
+                          senderImage: UIImage(systemName: "doc.fill")!, // 이거 ! 가 아니라 다른걸로 해줄거임..!
+                          messageContext: "예 암 뻐킹 톰보이",
+                          messageTime: Date())
+        )
+
+    }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return username.count
+        return messageList.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -32,11 +91,10 @@ class ChatListController: UIViewController, UITableViewDataSource, UITableViewDe
                 as? ChatCell else {
             return UITableViewCell()
         }
-
-        cell.username.text = username[indexPath.row]
-        cell.content.text = content[indexPath.row]
-        cell.time.text = time[indexPath.row]
-        cell.profileImage.image = UIImage(systemName: "doc.fill")
+        cell.username.text = messageList[indexPath.row].senderName + " 견주"
+        cell.content.text =  messageList[indexPath.row].messageContext
+        cell.time.text =  getChatTime(date: messageList[indexPath.row].messageTime)
+        cell.profileImage.image =  messageList[indexPath.row].senderImage
 
         return cell
     }
@@ -51,6 +109,8 @@ class ChatListController: UIViewController, UITableViewDataSource, UITableViewDe
         super.viewDidLoad()
         chatListView.delegate = self
         chatListView.dataSource = self
+
+        addList()
     }
 
 }
