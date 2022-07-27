360eaa4efda2c533a886aa4936b4067723266bca
Hongjaaa <102945279+Hongjaaa@users.noreply.github.com>
2022-07-27 13:39:08 +0900

    textviewcell 정리

--- UDD/Presentation/Main/View/Like/LikeListView.swift
+++ UDD/Presentation/Main/View/Like/LikeListView.swift
@@ -2,13 +2,18 @@
 //  LikeListView.swift
 //  UDD
 //
-//  Created by 최홍준 on 2022/07/26.
+//  Created by 최홍준 on 2022/07/27.
 //
 
 import UIKit
 
-class LikeListView: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
+class LikeListView: UITableViewCell {
+    @IBOutlet weak var opponentImage: UIImageView!
+    @IBOutlet weak var opponentLabel1: UILabel!
+    @IBOutlet weak var opponentLabel2: UILabel!
+    
+    func configure(name: String, imageName: String) {
+        opponentLabel1.text = name
+        opponentImage.image = UIImage(named: imageName)
     }
 }
