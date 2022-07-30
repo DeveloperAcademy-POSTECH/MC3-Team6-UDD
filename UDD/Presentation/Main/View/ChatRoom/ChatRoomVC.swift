ad8a58354aad9da1d6cfba96e4b88d1b909d342d
jsh9611 <jangsh9611@naver.com>
2022-07-30 17:43:36 +0900

    [feat] send chatRoom data to chatList & refactor

--- UDD/Presentation/Main/View/ChatRoom/ChatRoomVC.swift
+++ UDD/Presentation/Main/View/ChatRoom/ChatRoomVC.swift
@@ -7,8 +7,13 @@
 
 import UIKit
 
+protocol ChatRoomVCDelegate: AnyObject {
+    func userChatData(lastText: String?, lastTime: Date?)
+}
+
 class ChatRoomVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextViewDelegate {
     @IBOutlet weak var inputTextViewHeight: NSLayoutConstraint!
+
     @IBOutlet weak var chatTableView: UITableView! {
         didSet {
             chatTableView.delegate = self
@@ -19,7 +24,9 @@ class ChatRoomVC: UIViewController, UITableViewDelegate, UITableViewDataSource, 
 
     // MARK: - chatData: 채팅을 저장할 배열
     var chatData = [String]()
+    var lastText = ""
     var messageList: RecentMessage!
+    weak var delegate: ChatRoomVCDelegate?
 
     // 채팅을 입력받기 위한 텍스트 뷰
     @IBOutlet weak var inputTextView: UITextView! {
@@ -41,6 +48,7 @@ class ChatRoomVC: UIViewController, UITableViewDelegate, UITableViewDataSource, 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "\(messageList.senderName!)"
+        lastText = messageList.messageContext
 
         // MARK: - 네비게이션바 더보기 버튼
         navigationItem.rightBarButtonItem = UIBarButtonItem(
@@ -70,6 +78,14 @@ class ChatRoomVC: UIViewController, UITableViewDelegate, UITableViewDataSource, 
                          name: UIResponder.keyboardDidHideNotification, object: nil)
     }
 
+    // view가 해제될 예정일 때 이전 뷰로 데이터 전달
+    override func viewWillDisappear(_ animated: Bool) {
+        self.delegate?.userChatData(
+            lastText: lastText,
+            lastTime: Date()
+        )
+    }
+
     // MARK: - 상단에 더보기 버튼 구현
     // 더보기를 누르면 Action Sheet가 나온다
     @objc func openTapped() {
@@ -85,22 +101,27 @@ class ChatRoomVC: UIViewController, UITableViewDelegate, UITableViewDataSource, 
         guard let selected: String = action.title else { return }
         var message = "알림창 내용"
         var yesButton = "확인버튼 내용"
+
         switch selected {
+
         case "채팅방 나가기":
             message = "정말 채팅창을 나가실건가요?"
             yesButton = "나가기"
+
         case "신고하기":
             message = "정말 신고 하실건가요?\n신고한 유저와는 채팅을 하실 수 없습니다."
             yesButton = "신고하기"
+
         default:
             break
         }
+
         let alert = UIAlertController(title: selected, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "아니요", style: .default) { _ in
             print("취소버튼 누름")
         })
         alert.addAction(UIAlertAction(title: yesButton, style: .destructive) { _ in
-          print("\(yesButton) 에 대한 정보를 root View에서 받고 처리해야함")
+          print("\(yesButton) 버튼 입니다.")
             self.navigationController?.popViewController(animated: true)
         })
         self.present(alert, animated: true, completion: nil)
@@ -112,6 +133,7 @@ class ChatRoomVC: UIViewController, UITableViewDelegate, UITableViewDataSource, 
         let keyboardFrame = notiInfo[UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
         let height = keyboardFrame.size.height - self.view.safeAreaInsets.bottom
         let animationDuration = notiInfo[ UIResponder.keyboardAnimationDurationUserInfoKey] as! TimeInterval
+
         // 키보드 올라오는 애니메이션이랑 동일하게 텍스트뷰 올라가게 만들기.
         UIView.animate(withDuration: animationDuration) {
             self.inputViewBottomMargin.constant = height
@@ -127,46 +149,53 @@ class ChatRoomVC: UIViewController, UITableViewDelegate, UITableViewDataSource, 
             self.view.layoutIfNeeded()
         }
     }
+
     // 텍스트뷰에 있는 값이 chatData array에 들어간다
     @IBAction func chatSendButton(_ sender: Any) {
         if inputTextView.text != "" {
             chatData.append(inputTextView.text)
+            lastText = inputTextView.text
             inputTextView.text = ""
         } else { return }
+
         let lastindexPath = IndexPath(row: chatData.count - 1, section: 0)
+
         chatTableView.insertRows(at: [lastindexPath], with: UITableView.RowAnimation.automatic)
         inputTextViewHeight.constant = 35
         chatTableView.scrollToRow(at: lastindexPath, at: UITableView.ScrollPosition.bottom, animated: true)
-        /* 티키타카를 하기 위한 공간 */
     }
-    // 테이블 뷰를 사용하게 되면 무조건 구현해야하는 함수 2개.
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return chatData.count
     }
-    // 원래는 상대방이 썼을 때 상대방의 셀, 내가 썼을 때 내 셀이 나와야 하는데 이거는 그냥 번갈아가면서 나오게 하는 거임.
+    // 상대방의 셀, 내가 썼을 때 셀이 번갈아가며 나옴
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // 첫번째로 보여줄 셀
         if indexPath.row == 0 {
+
             let firstCell = tableView
                 .dequeueReusableCell(withIdentifier: "chatInfoCell", for: indexPath) as! ChatInfoCell
+            firstCell.chatImage.image = UIImage(named: "golddog")
+            firstCell.todayDate.text = nowKorDate()
             firstCell.selectionStyle = .none
             return firstCell
         } else if indexPath.row % 2 == 1 {
-            // MyCell 형식으로 사용하기 위해 형변환이 필요하다.
+
             let myCell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as! MyCell
-            // 버튼 누르면 chatData 에 텍스트를 넣을 것이기 때문에 거기서 꺼내오면 되는거다.
             myCell.myTextView.text = chatData[indexPath.row]
             myCell.mySendTime.text = nowKorTime()
             myCell.selectionStyle = .none
             return myCell
         } else {
             let yourCell = tableView.dequeueReusableCell(withIdentifier:"yourCell",for:indexPath) as! YourCell
+
             yourCell.yourTextView.text = chatData[indexPath.row]
             yourCell.yourReceiveTime.text = nowKorTime()
             yourCell.selectionStyle = .none
             return yourCell
         }
     }
+
     func textViewDidChange(_ textView: UITextView) {
         if textView.contentSize.height <= 35 {
             inputTextViewHeight.constant = 35
@@ -176,6 +205,7 @@ class ChatRoomVC: UIViewController, UITableViewDelegate, UITableViewDataSource, 
             inputTextViewHeight.constant = textView.contentSize.height
         }
     }
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
          self.view.endEditing(true)
    }
