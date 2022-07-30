aeb394167d42b5ad3033fe1ba015f622599bda44
J2in <jiin62622@gmail.com>
2022-07-30 22:59:37 +0900

    [feat] add onboarding-dogdetail page

---
+++ UDD/Presentation/OnBoarding/View/AddDogDetail/AddDogDetailController.swift
@@ -0,0 +1,18 @@
+//
+//  AddDogDetailController.swift
+//  UDD
+//
+//  Created by jiin on 2022/07/30.
+//
+
+import UIKit
+
+class AddDogDetailController: UIViewController {
+
+    
+    @IBOutlet weak var DogBreedTextField: CustomTextFields!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
